a4314f0dea86d9e954e3f91f8b0e08cf8d7dd5a7
Oskar Mendel <brokenprogrammer@gmail.com>
2017-09-01 01:50:41 +0200

    Added type definitions to omtools single header library.

--- CPP/omtools.h
+++ CPP/omtools.h
@@ -5,5 +5,24 @@
  */
 
 //TODO: Add string and file reading utilities.
-//TODO: Read https://github.com/nothings/stb/blob/master/stb.h
-//      for guidance.
+//TODO: Date & time utility functions.
+
+///////////////////////////////////////////////////////////////////////////////
+//  typedefs - Type definitions for common types.
+//
+
+typedef unsigned char      byte;
+typedef unsigned char      uint8;
+typedef signed   char      int8;
+typedef unsigned short int uint16;
+typedef signed   short int int16;
+typedef unsigned int       uint32;
+typedef signed   int       int32;
+typedef unsigned long long uint64;
+typedef long long          int64;
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Strings - Helper functions defined to help
+//            working with strings.
+//
