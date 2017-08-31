c15ce1d22d51e663ecf0d6aa10f05c9808101f4f
Oskar Mendel <brokenprogrammer@gmail.com>
2017-08-31 21:58:26 +0200

    Preparation of implementation of single header utilities.

---
+++ CPP/omtools.h
@@ -0,0 +1,9 @@
+/*
+ * omtools.h
+ * This is a single header file with utilities
+ * for C/C++
+ */
+
+//TODO: Add string and file reading utilities.
+//TODO: Read https://github.com/nothings/stb/blob/master/stb.h
+//      for guidance.
