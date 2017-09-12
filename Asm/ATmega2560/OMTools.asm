eedea01024c947444e8156118af1c124b5a2848e
Oskar Mendel <brokenprogrammer@gmail.com>
2017-09-13 00:21:38 +0200

    Created library function Square for OMTools.asm

--- Asm/ATmega2560/OMTools.asm
+++ Asm/ATmega2560/OMTools.asm
@@ -6,6 +6,8 @@
 ; the stack as well as helper functions for different kinds
 ; of tasks such as math.
 ;
+; Uses the three registers r23, r24 and r25 for its subroutines.
+;
 ; Oskar Mendel
 ; 09/12/2017
 
@@ -17,4 +19,25 @@ out SPH, r16						; SPH = High part of RAMEND address.
 ldi r16, LOW(RAMEND)				; r16 = Low part of RAMEND address.
 out SPL, r16						; SPL = Low Part of RAMEND address.
 
+.DEF OMTools_X = r23
+.DEF OMTools_Y = r24
+.DEF OMTools_Z = r25
+
 rjmp Main							; Jumping to Main.
+
+;**************************************************
+; Square
+; This function uses the register 23 which is defined
+; as OMTools_X and squares the value within it then
+; stores the result back in OMTools_X
+;
+; @param OMTools_X - Value to square.
+;**************************************************
+OMTools_Square:
+	mul OMTools_X, OMTools_X		; Square OMTools_X
+	mov OMTools_X, r0				; Move the result back into OMTools_X
+	
+	clr r0							; Clear the result from the multiplication from r0.
+	clr r1							; Clear the result from the multiplication from r1.
+	
+	ret
