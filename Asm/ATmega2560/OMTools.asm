4cbffe821a632ec15c41ef70140833ed0ba7b341
Oskar Mendel <brokenprogrammer@gmail.com>
2017-09-13 19:11:00 +0200

    Added square root function to OMTools.asm

--- Asm/ATmega2560/OMTools.asm
+++ Asm/ATmega2560/OMTools.asm
@@ -19,6 +19,10 @@ out SPH, r16						; SPH = High part of RAMEND address.
 ldi r16, LOW(RAMEND)				; r16 = Low part of RAMEND address.
 out SPL, r16						; SPL = Low Part of RAMEND address.
 
+
+;**************************************************
+; OMTools REGISTER DEFINITIONS
+;**************************************************
 .DEF OMTools_X = r23
 .DEF OMTools_Y = r24
 .DEF OMTools_Z = r25
@@ -40,4 +44,54 @@ OMTools_Square:
 	clr r0							; Clear the result from the multiplication from r0.
 	clr r1							; Clear the result from the multiplication from r1.
 	
+	ret
+
+;**************************************************
+; Square root
+; This function uses the fact that the sum of odd 
+; numbers create square numbers. Values like 11 that
+; doesn't have an integer as its square root will be
+; truncated.
+;
+; Example:
+;	1         = 1^2 = 1
+;	1 + 3     = 2^2 = 4
+;	1 + 3 + 5 = 3^2 = 9 
+;
+; @param OMTools_X - Value to take the square root of.
+;**************************************************
+OMTools_Squareroot:
+	ldi OMTools_Y, 1				; Used for sum.
+
+	OMTools_SquarerootLoop:
+		sub OMTools_X, OMTools_Y	; Subtract the square with our current sum.
+		brcs OMTools_SquarerootDone ; If carry flag got set by subtraction we are done.
+		inc r0						; Increment counter.
+		subi OMTools_Y, -2			; Get next odd number.
+		rjmp OMTools_SquarerootLoop
+
+	OMTools_SquarerootDone:
+		mov OMTools_X, r0			; Move counter result to OMTools_X
+
+		clr OMTools_Y				; Clear the helper registers.
+		clr r0
+
+		ret
+
+;**************************************************
+; Multiply single byte number.
+;
+; 
+;**************************************************
+OMTools_Mul8:
+
+	ret
+
+;**************************************************
+; Divide single byte number.
+;
+; 
+;**************************************************
+OMTools_Div8:
+
 	ret
