1fcc4b4adc71ac7127e4087632e01c533942655d
Oskar Mendel <brokenprogrammer@gmail.com>
2018-09-26 00:56:40 +0200

    initial commit IssueReporter

---
+++ Productivity/IssueReporter/README.md
@@ -0,0 +1,2 @@
+# IssueReporter
+Small tool that looks for TODOs in source code and reports them as Issues on github.
---
+++ Productivity/IssueReporter/build.bat
@@ -0,0 +1,6 @@
+@echo off
+
+mkdir build
+pushd build
+cl -Zi C:\Users\Oskar\Documents\GitHub\tools\Productivity\IssueReporter\src\main.c
+popd
